c5d7a0c7064ec3cf1c000c6aa1e1ad75e85e71b8
M-Sai-Kiran <saisravankiran@gmail.com>
2020-05-13 23:42:45 +0530

    practice app done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { increment, decrement, loginToggle } from "./Actions/action";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter</h1> {counter}
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(loginToggle())}>Login Toggle</button>
+      {isLogged ? (
+        <h3>The User is logged in.</h3>
+      ) : (
+        <h3>The User is not logged in.</h3>
+      )}
     </div>
   );
 }
